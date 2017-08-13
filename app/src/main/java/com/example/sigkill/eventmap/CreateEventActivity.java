f6ee4d3b7ee24aa1c4f2644ec5e8ab191aa15287
natalyturbina <turbinanataly@gmail.com>
2017-08-13 13:45:28 +0300

    add database fucnctionality

--- app/src/main/java/com/example/sigkill/eventmap/CreateEventActivity.java
+++ app/src/main/java/com/example/sigkill/eventmap/CreateEventActivity.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
 //import com.google.firebase.database.DataSnapshot;
 //import com.google.firebase.database.DatabaseError;
 //import com.google.firebase.database.DatabaseReference;
@@ -40,6 +45,7 @@ public class CreateEventActivity extends AppCompatActivity {
         Event event = new Event();
         View parent = (View)view.getParent();
         if (parent != null) {
+            event.id = UUID.randomUUID();
             TextView txtView = parent.findViewById(R.id.editText4);
             event.Title = txtView.getText().toString();
 
@@ -48,9 +54,16 @@ public class CreateEventActivity extends AppCompatActivity {
 
             event.Coordinates = getIntent().getParcelableExtra("latlng");
 
-            EventSaver.getInstance().store_event(event);
+            DatabaseReference myRef = FirebaseDatabase.getInstance().getReference().child("events");
 
+            myRef.child(event.id.toString()).child("id").setValue(event.id.toString());
+            myRef.child(event.id.toString()).child("title").setValue(event.Title);
+            myRef.child(event.id.toString()).child("desc").setValue(event.Description);
+            myRef.child(event.id.toString()).child("coord").setValue(event.Coordinates.toString());
+
+                    //EventSaver.getInstance().store_event(event);
+            //Toast.makeText(getApplication(), "123123", Toast.LENGTH_LONG).show();
         }
-        Toast.makeText(getApplication(), "123123", Toast.LENGTH_LONG).show();
+
     }
 }
--- app/src/main/java/com/example/sigkill/eventmap/Event.java
+++ app/src/main/java/com/example/sigkill/eventmap/Event.java
@@ -4,12 +4,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by sigkill on 8/12/17.
  */
 
 public class Event {
+    UUID id;
     public Date DateTime;
     public String Title;
     public String Description;
